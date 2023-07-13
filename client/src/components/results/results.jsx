3c5e0511062ded91ccdd91e7018b12b820e90e86
NessDisk <ness.cero1@gmail.com>
2023-07-13 00:58:41 -0500

    Results styles

--- client/src/components/results/results.jsx
+++ client/src/components/results/results.jsx
@@ -1,9 +1,74 @@
 import React from 'react'
 import './results.scss'
+import Product_Test from '../../assets/Product_Test.jpg'
 
 function results() {
   return (
-    <div className='container'>results</div>
+    <div className='container'>
+      <div className='dir_text' >dir →</div>
+      <div className='resultSearch'>
+        <div className='Reslut_info'>
+          <img className='result_img' src={Product_Test}/>
+          <div className='result_metaData' >
+            
+            <div className='result_value'>
+                <h2 className='result_numValue'>
+                  value 0000$  
+                </h2>
+                <div className='result_pointValue'/>
+            </div>
+
+            <div className='result_characterist'>
+              xxx xxx xxxx xxx xxx xxx xxx xxx xxxx xxxx xxxxx xxxx xxxx xxxx xxxx xxxx xxxx xxxx xxxx
+            </div>
+
+          </div>          
+          <div className='result_extra'> extra</div>
+        </div>
+
+      {/*    */}
+
+      <div className='Reslut_info'>
+          <img className='result_img' src={Product_Test}/>
+          <div className='result_metaData' >
+            
+            <div className='result_value'>
+                <h2 className='result_numValue'>
+                  value 0000$  
+                </h2>
+                <div className='result_pointValue'/>
+            </div>
+
+            <div className='result_characterist'>
+              xxx xxx xxxx xxx xxx xxx xxx xxx xxxx xxxx xxxxx xxxx xxxx xxxx xxxx xxxx xxxx xxxx xxxx
+            </div>
+
+          </div>          
+          <div className='result_extra'> extra</div>
+        </div>
+ {/*    */}
+
+ <div className='Reslut_info'>
+          <img className='result_img' src={Product_Test}/>
+          <div className='result_metaData' >
+            
+            <div className='result_value'>
+                <h2 className='result_numValue'>
+                  value 0000$  
+                </h2>
+                <div className='result_pointValue'/>
+            </div>
+
+            <div className='result_characterist'>
+              xxx xxx xxxx xxx xxx xxx xxx xxx xxxx xxxx xxxxx xxxx xxxx xxxx xxxx xxxx xxxx xxxx xxxx
+            </div>
+
+          </div>          
+          <div className='result_extra'> extra</div>
+        </div>
+
+      </div>
+    </div>
   )
 }
 
