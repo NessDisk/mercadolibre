19807ca9c7fc6b1231ed30252cffad485259c0d2
NessDisk <ness.cero1@gmail.com>
2023-08-07 19:26:43 -0500

    servidor express

---
+++ client/src/server/app.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const app = express();
+const PORT = 3100;
+const axios = require('axios');
+const cors = require('cors');
+
+
+app.use(express.json(), cors({ origin: 'http://localhost:3000' }));
+// Get roter mercadoLibre
+app.get('/api/search', async (req, res) => {
+  
+ 
+  const response = await axios.get('https://api.mercadolibre.com/sites/MLA/search?q=:'+req.query.query);
+ 
+
+  const product = response.data;
+    const query = req.query.query;
+  const data = { message: query };
+
+
+
+  console.log(product);
+  res.json(product);
+});
+
+//details
+
+app.get('/api/items', async (req, res) => {
+  
+ 
+  const response = await axios.get('https://api.mercadolibre.com/items/'+req.query.id);
+  const descriptionjson = await axios.get('https://api.mercadolibre.com/items/'+req.query.id+'/description');
+
+  const product = response.data;
+  const description = descriptionjson.data;
+    const query = req.query.id;
+  const data = { message: query };
+
+  const finalData = {
+    "author": {
+    "name": "Nestor",
+    "lastname": "Solera"
+    },
+    "item": {
+    "id": product.id,
+    "title": product.title,
+    "price": {
+    "currency": product.currency_id,
+    "amount": product.price,
+    "decimals": product.base_price,
+    },
+    "picture": product["pictures"][0]["url"],
+    "condition": product["condition"],
+    "free_shipping":  product["shipping"]["free_shipping"],
+    "sold_quantity": product["sold_quantity"],
+    "description": description["plain_text"],
+    "attributes":product["attributes"],
+    }
+    }
+
+
+  console.log(finalData);
+  res.json(finalData);
+});
+
+
+
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en http://localhost:${PORT}`);
+});
--- client/src/store/actions/actions.js
+++ client/src/store/actions/actions.js
@@ -10,7 +10,7 @@ export const setEndpoints = (searchValue ) => {
 
 return (dispatch) => {
     // Realizar la solicitud a la API para obtener los endpoints
-    fetch('https://api.mercadolibre.com/sites/MLA/search?q=:'+searchValue)
+    fetch('http://localhost:3100/api/search?query='+searchValue)
       .then((response) => response.json())
       .then((data) => {
         // Dispatchear la acción con los datos obtenidos
@@ -36,47 +36,20 @@ export const GetItenIddetails = (idDetails ) => {
 
   return (dispatch) => {
       // Realizar la solicitud a la API para obtener los endpoints
-      fetch('https://api.mercadolibre.com/items/'+idDetails)
+      fetch('http://localhost:3100/api/items?id='+idDetails)
         .then((response) => response.json())
-        .then((data) => {
-          // Dispatchear la acción con los datos obtenidos
-
-          fetch('https://api.mercadolibre.com/items/'+idDetails+'/description')
-          .then((response) => response.json())
-          .then((datadescription) => {
-
-            const finalData = {
-              "author": {
-              "name": "Nestor",
-              "lastname": "Solera"
-              },
-              "item": {
-              "id": data.id,
-              "title": data.title,
-              "price": {
-              "currency": data.currency_id,
-              "amount": data.price,
-              "decimals": data.base_price,
-              },
-              "picture": data["pictures"][0]["url"],
-              "condition": data["condition"],
-              "free_shipping":  data["shipping"]["free_shipping"],
-              "sold_quantity": data["sold_quantity"],
-              "description": datadescription["plain_text"]
-              }
-              }
-              
-
+        .then((data) => {              
+            console.log("detail", data.item.attributes);
               dispatch({
                 type: GET_ITEN_DETAILS,
-                payload: finalData,
+                payload: data,
               });
               dispatch({
                 type: BREADCRUMB_DETAILS,
-                payload: data.attributes,
+                payload:  data.item.attributes,
               });
              
-            })
+          
         })
         .catch((error) => {
           console.error('Error al obtener los datos:', error);
