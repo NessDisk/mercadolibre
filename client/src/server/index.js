8a205e9135d5055c257510ffd26c99e5e2980eb9
NessDisk <ness.cero1@gmail.com>
2023-08-10 14:57:06 -0500

    express server vercel

--- client/src/server/index.js
+++ client/src/server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 3100;
+const PORT = process.env.PORT || 3100  ;
 const axios = require('axios');
 const cors = require('cors');
 
